b17c29a21ac0b84fde5d46f327def1ee71a5d2a8
kitaePark <bkt4044@naver.com>
2024-01-27 18:04:23 +0900

    Feat: filter 기능 & calculate 기능 추가

--- DOM Array Methods/script.js
+++ DOM Array Methods/script.js
@@ -3,7 +3,7 @@ const addUserBtn = document.getElementById("add-user");
 const doubleBtn = document.getElementById("double");
 const showMillionairesBtn = document.getElementById("show-millionaires");
 const sortBtn = document.getElementById("sort");
-const calculateWealthBtn = document.getElementById("calcultae-wealth");
+const calculateWealthBtn = document.getElementById("calculate-wealth");
 
 let data = [];
 
@@ -42,11 +42,31 @@ function sortByRichest() {
   updateDOM();
 }
 
+// Filter only millionaires
+function showMillionaires() {
+  data = data.filter((user) => {
+    user.money > 1000000;
+  });
+
+  updateDOM();
+}
+
 // Add new obj to data arr
 function addData(obj) {
   data.push(obj);
 
   updateDOM();
+}
+
+// Calculate the total wealth
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+  const wealthEl = document.createElement("div");
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
+    wealth
+  )}</strong></h3>`;
+  main.appendChild(wealthEl);
 }
 
 // Update DOM
@@ -73,3 +93,5 @@ function formatMoney(number) {
 addUserBtn.addEventListener("click", getRandomUser);
 doubleBtn.addEventListener("click", doubleMoney);
 sortBtn.addEventListener("click", sortByRichest);
+showMillionairesBtn.addEventListener("click", showMillionaires);
+calculateWealthBtn.addEventListener("click", calculateWealth);
