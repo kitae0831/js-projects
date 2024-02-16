72100f3c1eceefac87a54550d84c062ac1d7e3ff
kitaePark <bkt4044@naver.com>
2024-02-16 23:35:18 +0900

    Feat: input에 event 설정 및 정답을 맞추면 score가 올라가게 끔 JS 완료

--- Typing Game/script.js
+++ Typing Game/script.js
@@ -31,3 +31,44 @@ const words = [
   "drag",
   "loving",
 ];
+
+// Init word
+let randomWord;
+
+// Init score
+let score = 0;
+
+// Init imte
+let time = 10;
+
+//Generate random word from array
+function getRandomWord() {
+  return words[Math.floor(Math.random() * words.length)];
+}
+
+// Add word to DOM
+function addWordToDOM() {
+  randomWord = getRandomWord();
+  word.innerHTML = randomWord;
+}
+
+// Update Score
+function updateScore() {
+  score++;
+  scoreEl.innerHTML = score;
+}
+
+addWordToDOM();
+
+// Event Listeners
+text.addEventListener("input", (e) => {
+  const insertedText = e.target.value;
+
+  if (insertedText === randomWord) {
+    addWordToDOM();
+    updateScore();
+
+    // Clear
+    e.target.value = "";
+  }
+});
