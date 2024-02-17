2a3c35069dd8c71385dfd746b91a0a9426eec05c
kitaePark <bkt4044@naver.com>
2024-02-17 23:47:22 +0900

    Upload: img 파일 추가

---
+++ Speech Text Reader/script.js
@@ -0,0 +1,13 @@
+const main = document.querySelector("main");
+const voiceSelect = document.getElementById("voices");
+const text = document.getElementById("text");
+const readBtn = document.getElementById("read");
+const toggleBtn = document.getElementById("toggle");
+const closeBtn = document.getElementById("close");
+
+const data = [
+  {
+    image: "./img/drink.jpg",
+    text: "I'm Thirsty",
+  },
+];
