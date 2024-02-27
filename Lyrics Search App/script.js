7f474dba8fb534f4bb1789df6c2765a30b1b9104
kitaePark <bkt4044@naver.com>
2024-02-28 00:48:55 +0900

    Feat: Lyrics api연동으로 가사 연결

--- Lyrics Search App/script.js
+++ Lyrics Search App/script.js
@@ -22,7 +22,7 @@ function showData(data) {
         (song) => `
         <li>
             <span><strong>${song.artist.name}</strong> - ${song.title}</span>
-            <button class="btn" data-artist="${song.artist.name}">Get Lyrics</button>
+            <button class="btn" data-artist="${song.artist.name}" data=songtitle="${song.title}">Get Lyrics</button>
         </li>`
       )
       .join("")}
@@ -55,6 +55,19 @@ async function getMoreSongs(url) {
   showData(data);
 }
 
+// Get lyrics for song
+async function getLyrics(artist, songTitle) {
+  const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+  const data = await res.json();
+
+  const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, "<br>");
+
+  result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+  <span>${lyrics}</span>`;
+
+  more.innerHTML = "";
+}
+
 // Event listeners
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -65,5 +78,17 @@ form.addEventListener("submit", (e) => {
     alert("Please type in a search term");
   } else {
     searchSongs(searchTerm);
+  }
+});
+
+// Get lyrics button click
+result.addEventListener("click", (e) => {
+  const clickedEl = e.target;
+
+  if (clickedEl.tagName === "BUTTON") {
+    const artist = clickedEl.getAttribute("data-artist");
+    const songTitle = clickedEl.getAttribute("data-songtitle");
+
+    getLyrics(artist, songTitle);
   }
 });
